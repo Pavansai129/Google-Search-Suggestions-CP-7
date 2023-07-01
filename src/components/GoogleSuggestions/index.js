2e6d8dd2dec9c2d60277b8c6a1c27abaed1d294e
PavanSai <pavansaivvit@gmail.com>
2023-07-01 20:25:04 +0000

    all testcases passed

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -6,12 +6,25 @@ import './index.css'
 class GoogleSuggestions extends Component {
   state = {searchInput: ''}
 
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  onSelectingSuggestion = suggestion => {
+    this.setState({searchInput: suggestion})
+  }
+
   render() {
     const {searchInput} = this.state
+    const input = searchInput.toLowerCase()
     const {suggestionsList} = this.props
-    const filteredSuggestionsList = suggestionsList.filter(each =>
-      each.suggestion.includes(searchInput),
-    )
+    const filteredSuggestionsList = suggestionsList.filter(each => {
+      const search = each.suggestion.toLowerCase()
+      if (search.includes(input)) {
+        return true
+      }
+      return false
+    })
     return (
       <div className="app-bg-container">
         <div className="app-container">
@@ -25,12 +38,20 @@ class GoogleSuggestions extends Component {
               src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
               alt="search icon"
             />
-            <input type="search" value={searchInput} />
+            <input
+              type="search"
+              value={searchInput}
+              onChange={this.onChangeSearchInput}
+            />
           </div>
           <div className="suggestions">
             <ul>
               {filteredSuggestionsList.map(each => (
-                <SuggestionItem key={each.id} suggestion={each.suggestion} />
+                <SuggestionItem
+                  key={each.id}
+                  suggestion={each.suggestion}
+                  onSelectingSuggestion={this.onSelectingSuggestion}
+                />
               ))}
             </ul>
           </div>
--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,9 +1,17 @@
 // Write your code here
 const SuggestionItem = props => {
-  const {suggestion} = props
+  const {suggestion, onSelectingSuggestion} = props
+  const onArrowClick = () => {
+    onSelectingSuggestion(suggestion)
+  }
   return (
     <li>
       <p>{suggestion}</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+        alt="arrow"
+        onClick={onArrowClick}
+      />
     </li>
   )
 }
